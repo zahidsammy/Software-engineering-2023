0f3f3230cf87e57372151ea5769417ce9f131b98
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-10-11 17:33:50 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+function Sayhello(name) {
+    console.log('Hello '+name)
+    
+    
+}
+
+Sayhello("Zahid")
+
+
+function sum(a,b){
+    console.log(a + b)
+}
+
+function minus(a,b){
+    console.log(a - b)
+}
+
+function multiply(a,b){
+    console.log(a * b)
+}
+
+function divide(a,b){
+    console.log(a / b)
+}
+
+sum(5, 5)
+
+minus(5, 5)
+
+multiply(5, 5)
+
+divide(5, 5)
+
+
