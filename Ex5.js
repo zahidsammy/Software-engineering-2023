99c0d77865ef9da24897135faa0cb03270a04497
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-10-12 17:24:32 +1300

    Add files via upload

---
+++ Ex5.js
@@ -0,0 +1,11 @@
+let ShoppingCart = ['Egg' , 'Bread' , 'Milk' , 'Whey Protein' , 'Rice'];
+
+ShoppingCart[1]='Thai Food'
+
+ShoppingCart[4]='Orange'
+
+ShoppingCart.unshift('plum')
+
+ShoppingCart.pop()
+
+console.log(ShoppingCart)
