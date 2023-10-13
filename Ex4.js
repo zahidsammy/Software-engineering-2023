428c1dbe43a8d8bd2e190c37cc338bc42dbdf4af
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-10-13 15:03:18 +1300

    Add files via upload

---
+++ Ex4.js
@@ -0,0 +1,26 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+function divide(a, b) {
+    return a / b
+}
+
+
+console.log("Addition:", add(5, 5));       
+console.log("Subtraction:", subtract(5, 5));  
+console.log("Multiplication:", multiply(5, 5));  
+console.log("Division:", divide(5, 5));    
+console.log("Division:", divide(5, 5));
